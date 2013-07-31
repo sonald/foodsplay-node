dd8217b89f8095201c4426ae38c7758897728c9f
Sian Cao <sycao@redflag-linux.com>
2013-07-31 14:46:21 +0800

    mod for nodejitsu

--- app.js
+++ app.js
@@ -11,16 +11,7 @@ var express = require('express'),
 
 // use default connection
 var localmode = process.env.USER == 'sonald';
-if (localmode) {
-    console.log('connecting local mongodb');
-    mongoose.connect('mongodb://127.0.0.1/foodsplay');
-} else {
-    var db_url = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL
-            || 'mongodb://nodejitsu:3329971a489a0c260559b594a747dd39@linus.mongohq.com:10039/nodejitsudb3477517154';
-            //|| 'mongodb://nodejitsu:715a0b47f2b374f36b3ba71983388bac@alex.mongohq.com:10003/nodejitsudb1701304151';
-    console.log('connecting ', db_url);
-    mongoose.connect(db_url);
-}
+
 
 mongoose.connection.once('open', function() {
     console.log('connected to database');
@@ -29,22 +20,17 @@ mongoose.connection.once('open', function() {
     require('./config')(app, express);
     routes.setup(app);
 
-    if (!localmode) {
-        // this self-signed pem now
-        var https_opts = {
-            key: fs.readFileSync('foodsplay.key'),
-            cert: fs.readFileSync('foodsplay-ca.pem')
-        };
-
-        https.createServer(https_opts, app).listen(app.get('port'), function(){
-            console.log("https server listening on port " + app.get('port'));
-        });
-
-
-    } else {
-        http.createServer(app).listen(app.get('port'), function(){
-            console.log("http server listening on port " + app.get('port'));
-        });
-    }
-
+    http.createServer(app).listen(app.get('port'), function(){
+        console.log("http server listening on port " + app.get('port'));
+    });
 });
+
+if (localmode) {
+    console.log('connecting local mongodb');
+    mongoose.connect('mongodb://127.0.0.1/foodsplay');
+} else {
+    var db_url = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL
+		|| 'mongodb://nodejitsu:24ff315588c0c41ca75fd3a5cc0bbd1a@dharma.mongohq.com:10061/nodejitsudb7191831402';
+    console.log('connecting ', db_url);
+    mongoose.connect(db_url);
+}
--- config.js
+++ config.js
@@ -20,7 +20,7 @@ module.exports = function(app, express) {
     require('./routes/auth');
 
     app.configure(function(){
-        app.set('port', process.env.PORT || 3000);
+        app.set('port', process.env.PORT || 80);
         app.set('views', __dirname + '/views');
         app.set('view engine', 'jade');
         app.use(express.favicon());
